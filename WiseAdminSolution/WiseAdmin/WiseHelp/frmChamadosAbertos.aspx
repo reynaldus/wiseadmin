76b8fcfe3dde16081c6da84a99ac70c7e0f7b48d
Francisco S. Pereira <fdspereira@terra.com.br>
2012-09-29 19:52:50 -0300

    Alterado Layout mais novas funcionalidades

---
+++ WiseAdminSolution/WiseAdmin/WiseHelp/frmChamadosAbertos.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WiseHelp/WiseHelp.master" AutoEventWireup="true" CodeBehind="frmChamadosAbertos.aspx.cs" Inherits="WiseAdmin.WiseHelp.frmChamadosAbertos" %>
+<%@ MasterType VirtualPath="~/WiseHelp/WiseHelp.master"  %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPP" runat="server">
+</asp:Content>
---
+++ WiseAdminSolution/WiseAdmin/WiseHelp/frmChamadosAbertos.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WiseAdmin.WiseHelp
+{
+    public partial class frmChamadosAbertos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
